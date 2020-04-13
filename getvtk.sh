db374382e5ae2678f66cef1ba6afe89bce8c6153
Vykstorm <victorruizgomezdev@gmail.com>
2020-04-13 14:56:18 +0200

    Updated bash script

--- getvtk.sh
+++ getvtk.sh
@@ -1,10 +1,11 @@
 
 OUTPUT_DIR=.
 DOCKER_IMAGE_NAME=vtk_tk_support
-docker build . -t $DOCKER_IMAGE_NAME
-docker run vtk_tk_support
-DOCKER_CONTAINER=$(docker ps -l -q)
-docker cp $DOCKER_CONTAINER:/dist/ ./dist
-docker rm $DOCKER_CONTAINER
-echo "Succesfully created wheels for vtk! ( checkout dist directory )"
-ls | grep *.whl
+docker build . -t $DOCKER_IMAGE_NAME \
+&& docker run vtk_tk_support \
+&& DOCKER_CONTAINER=$(docker ps -l -q) \
+&& sudo rm -rf dist \
+&& docker cp $DOCKER_CONTAINER:/dist/ . \
+&& docker rm $DOCKER_CONTAINER \
+&& echo "Succesfully created wheels for vtk! ( checkout dist directory )" \
+&& ls | grep *.whl \
